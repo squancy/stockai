822a852c323cb3481524492b1233ff7ee1e0dcd9
squancy <52940084+squancy@users.noreply.github.com>
2019-07-16 11:53:37 +0200

    Add files via upload

---
+++ downloads.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <title>Stock AI</title>
+        <meta charset="utf-8">
+        <meta lang="en">
+		<link rel="stylesheet" type="text/css" href="/style/style.css">
+        <link rel="icon" type="image/x-icon" href="/images/favstock.png">
+        <meta name="description" content="A legnagyobb volatilitású magyar részvények analizálárása szolgáló open-source program.">
+    </head>
+    <body>
+		<header>
+			<a href="/test.php">Stock AI</a>
+			<div class="headerRight">
+				<a href="/downloads.php">Downloads</a>
+				<a href="/cont.php">Contribute</a>
+				<a href="/about.php">About</a>
+			</div>
+		</header>
+		<br><br><br><br>	
+        <div class="hStocks nonStockCont">
+			<div class="stockCont infoHolder" style="border-radius: 5px;">
+				<b class="heading">1. Source code variations</b>
+				<b>&emsp;1.1 Download files separately</b>
+				<p>&emsp;&emsp;<a href="/test.php" download="test.php">index.php</a> (22 KB)</p>
+				<p>&emsp;&emsp;<a href="/downloads.php" download="downloads.php">downloads.php</a> (1 KB)</p>
+				<p>&emsp;&emsp;<a href="/cont.php" download="cont.php">contribute.php</a> (1.6 KB)</p>
+				<p>&emsp;&emsp;<a href="/about.php" download="about.php">about.php</a> (8 KB)</p>
+				<p>&emsp;&emsp;<a href="/template_pageBottom.php" download="template_pageBottom.php">template_pageBottom.php</a> (681 B)</p>
+				<p>&emsp;&emsp;<a href="/style/style_ai.css" download="style.css">style.css</a> (1.9 KB)</p>
+				<p>&emsp;&emsp;<a href="/images/favstock.png" download="favstock.png">favicon.png</a> (316 B)</p>
+				<p>&emsp;&emsp;<a href="/images/rolling.gif" download>rolling.gif</a> (17 KB)</p>
+				<p>&emsp;&emsp;<a href="/curl.php" download>curl.php</a> -- optional (1.6 KB)</p>
+				
+				<b>&emsp;1.2 Download files as a .tar or .zip file</b>
+				<p>&emsp;&emsp;<a href="/stockai.tar">stockai.tar</a> (128 KB)</p>
+				<p>&emsp;&emsp;<a href="/stockai.zip">stockai.zip</a> (48 KB)</p>
+				<b>&emsp;1.3 Visit the GitHub repository</b>
+				<p>&emsp;&emsp;Contribute to Stock AI via <a href="">GitHub</a>.</p>
+			</div>
+		</div>
+	</body>
+</html>	
