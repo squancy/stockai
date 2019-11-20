7711a55b5e9493f63b424a93a111ffc4afe77b5a
squancy <mark.frankli@pearscom.com>
2019-11-20 17:55:26 +0100

    Update stock_email.php

--- cron_jobs/stock_email.php
+++ cron_jobs/stock_email.php
@@ -6,7 +6,18 @@
     
     $data = getDataStock();
     $pieces = explode("|||", $data);
-    
+    $dateNow = new DateTime(date('Y-m-d H:i:s'));
+    $dateClose = new DateTime(date('Y-m-d 17:10:00'));
+    $dateOpen = new DateTime(date('Y-m-d 09:00:00'));
+    if($dateNow > $dateClose){
+        $zero = 0;
+        $sql = "UPDATE stockai_email SET count=?";
+		$stmt = $conn->prepare($sql);
+		$stmt->bind_param("i",$zero);
+		$stmt->execute();
+		$stmt->close();
+		exit();
+    }
     // -1 cuz we have a ||| at the end as well
     for($i = 0; $i < count($pieces) - 1; $i++){
         $final = json_decode($pieces[$i], true);
@@ -15,9 +26,7 @@
         $stock = $final['ticker'];
         if(($current / ($open / 100) >= 102) || ($current / ($open / 100) <= 98)){
             date_default_timezone_set('Europe/Budapest');
-            $dateNow = new DateTime(date('Y-m-d H:i:s'));
-            $dateClose = new DateTime(date('Y-m-d 17:10:00'));
-            if($dateNow > $dateClose){
+            if($dateNow >= $dateOpen && $dateNow <= $dateClose){
                 // Send email
                 $change = number_format(floatval($current / ($open / 100)), 2);
                 if($change < 100){
@@ -25,40 +34,47 @@
                 }else{
                     $change = '<span style="color: green">+'.($change - 100).'%</span>';
                 }
-                $sql = "SELECT email FROM stockai_email";
+                $sql = "SELECT email, count FROM stockai_email WHERE count < 7";
                 $stmt = $conn->prepare($sql);
                 $stmt->execute();
                 $result = $stmt->get_result();
                 while ($row = $result->fetch_assoc()) {
                     $e = $row["email"];
+                    $cnt = $row["count"];
+                    $cnt++;
                     $to = "$e";
         			$from = "Stock AI <stockai@pearscom.com>";
         			$subject = 'Stock AI - '.$stock;
-        			$message = '<!DOCTYPE html>
+        			$message = '
+        			<!DOCTYPE html>
                     <html>
                            <head>
-                                  <meta charset="UTF-8">
-                                  <title>Stock AI - '.$stock.'</title>
+                                <title>Stock AI - '.$stock.'</title>
+                    		    <link rel="stylesheet" type="text/css" href="https://www.pearscom.com/style/style_ai.css">
+                    		    <style>
+                    		        a, a:hover{
+                    		            color: red;
+                    		            text-decoration: none;
+                    		        }
+                    		    </style>
                            </head>
-                           <style type="text/css">
-                                  div > a:hover, a{
-                                         text-decoration: none;
-                                  }
                     
-                                  #link:hover{
-                                         background-color: #ab0000;
-                                  }
-                    
-                                  @media only screen and (max-width: 768px){
-                                         #atp{
-                                                width: 100% !important;
-                                         }
-                                  }
-                           </style>
-                           <body style="font-family: Arial, sans-serif; background-color: #fafafa; box-sizing: border-box; margin: 0 auto; margin-top: 10px; max-width: 800px;">
-                                <p>Hello, '.$stock.' has changed '.$change.'. <a href="https://www.pearscom.com/stockai">Check it now!</a></p>
+                           <body>
+                               <header>
+                    			<a href="https://www.pearscom.com/stockai">Stock AI</a>
+                    		    </header>
+                    		    <br><br><br><br>
+                    		    <div class="hStocks" id="hStocks" style="text-align: center;">
+                    		        <h1>Big movers of the day</h1>
+                                    <p>'.$stock.' has changed '.$change.'. <a href="https://www.pearscom.com/stockai">Check it now!</a></p>
+                                </div>
                            </body>
-                    </html>       ';
+                    </html>';
+                    $sql = "UPDATE stockai_email SET count=? WHERE email=? LIMIT 1";
+            		$stmt = $conn->prepare($sql);
+            		$stmt->bind_param("is",$cnt,$e);
+            		$stmt->execute();
+            		$stmt->close();
         			$headers = "From: $from\n";
         	        $headers .= "MIME-Version: 1.0\n";
         	        $headers .= "Content-type: text/html; charset=iso-8859-1\n";
