9403fa66b064e33a1613094496f4f106114c2cce
Mark Frankli <mark@pearscom.com>
2020-03-11 19:56:01 +0100

    final clean

--- email.js
+++ email.js
@@ -1,76 +1,77 @@
-function _(el){
-    return document.getElementById(el);
+function _(el) {
+  return document.getElementById(el);
 }
 
-if(localStorage.getItem('stock_email') == 'no' || !localStorage.getItem('stock_email')){
-    _('outerEmail').innerHTML = `
+if (localStorage.getItem('stock_email') == 'no' || !localStorage.getItem(
+    'stock_email')) {
+  _('outerEmail').innerHTML = `
         <input id="innerEmail" type="email" placeholder="Email address">
         <button id="innerBtn" class="submit">Submit</button>
         <div class="clear"></div>`;
-    _('emailText').innerHTML = `
+  _('emailText').innerHTML = `
       <p style="text-align: center;">
         Want to get notifications about your stocks? Enter your email address below!
       </p>
     `;
-        
-    _('innerBtn').addEventListener('click', async function handler(e){
-        var formData = new FormData();
-        formData.append('email_addr', '_');
-        formData.append('value', _('innerEmail').value);
-        
-        try{
-            const res = await fetch('main.php', {
-               method: 'post',
-               body: formData
-            });
-            let final = await res.text();
-            if(final == "success"){
-                _('status').innerHTML = `
+
+  _('innerBtn').addEventListener('click', async function handler(e) {
+    var formData = new FormData();
+    formData.append('email_addr', '_');
+    formData.append('value', _('innerEmail').value);
+
+    try {
+      const res = await fetch('main.php', {
+        method: 'post',
+        body: formData
+      });
+      let final = await res.text();
+      if (final == "success") {
+        _('status').innerHTML = `
                   <p class="green">
                     Email address has been successfully submitted.
                   </p>
                 `;
-                localStorage.setItem('stock_email', _('innerEmail').value);
-                _('outerEmail').innerHTML = "";
-                _('emailText').innerHTML = "";
-            }else{
-                _('status').innerHTML = '<p class="red">' + final + '</p>';
-            }
-        }catch(e){
-            _('status').innerHTML = '<p class="red">An error occurred.</p>'; 
-        }
-    });
-}else{
-    _('emailText').innerHTML = `
+        localStorage.setItem('stock_email', _('innerEmail').value);
+        _('outerEmail').innerHTML = "";
+        _('emailText').innerHTML = "";
+      } else {
+        _('status').innerHTML = '<p class="red">' + final + '</p>';
+      }
+    } catch (e) {
+      _('status').innerHTML = '<p class="red">An error occurred.</p>';
+    }
+  });
+} else {
+  _('emailText').innerHTML = `
       <p style="text-align: center;" id="unsub">
         <a href="#">Unsubscribe from newsletter</a>
       </p>`;
-    _('unsub').addEventListener('click', async function unsubscribe(e){
-        let formData = new FormData();
-        formData.append('unsub', localStorage.getItem('stock_email'));
-        try{
-            const res = await fetch('main.php', {
-                method: 'post',
-                body: formData
-            });
-            let final = await res.text();
-            if(final == "success"){
-                _('emailText').innerHTML = `
+  _('unsub').addEventListener('click', async function unsubscribe(e) {
+    let formData = new FormData();
+    formData.append('unsub', localStorage.getItem('stock_email'));
+    try {
+      const res = await fetch('main.php', {
+        method: 'post',
+        body: formData
+      });
+      let final = await res.text();
+      if (final == "success") {
+        _('emailText').innerHTML = `
                   <p class="green" style="text-align: center;">
                     You have successfully unsubscribed from the newsletter.
                   </p>`;
-                localStorage.setItem('stock_email', 'no')
-            }else{
-                _('emailText').innerHTML = `
+        localStorage.setItem('stock_email', 'no')
+      } else {
+        _('emailText').innerHTML = `
                   <p class="red" style="text-align: center;">${final}</p>
                 `;
-            }
-        }catch(e){
-            _('emailText').innerHTML = `
+      }
+    } catch (e) {
+      _('emailText').innerHTML = `
               <p class="red" style="text-align: center;">
                 An error occurred.
               </p>
             `;
-        }
-    });
+    }
+  });
 }
--- main.php
+++ main.php
@@ -4,53 +4,54 @@
 
     // Get stock data as a JSON file from an outer resource (Portfolio)
     if(isset($_POST["refresh"]) && $_POST["refresh"] == "now"){
-        $html = getDataStock();
-		echo $html;
-		exit();
+      $html = getDataStock();
+      echo $html;
+      exit();
 	}
 	
 	// Validate email address on server side
 	if(isset($_POST['email_addr'])){
-	    $email = $_POST['value'];
-	    $emailErr = "";
-	    
-	    $sql = "SELECT COUNT(id) FROM stockai_email WHERE email = ?";
-    	$stmt = $conn->prepare($sql);
-    	$stmt->bind_param("s",$email);
-    	$stmt->execute();
-    	$stmt->bind_result($emailCount);
-    	$stmt->fetch();
-    	$stmt->close();
-    	
-	    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $emailErr = "Invalid email format.";
-        }else if($emailCount > 0){
-            $emailErr = "Email already exists.";
-        }else{
-            $sql = "INSERT INTO stockai_email(email, count, time) VALUES(?,0,NOW())";
-			$stmt = $conn->prepare($sql);
-			$stmt->bind_param("s",$email);
-			$stmt->execute();
-			$stmt->close();	
-            echo "success";
-            exit();
-        }
-        echo $emailErr;
-	    exit();
+    $email = $_POST['value'];
+    $emailErr = "";
+    
+    $sql = "SELECT COUNT(id) FROM stockai_email WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s",$email);
+    $stmt->execute();
+    $stmt->bind_result($emailCount);
+    $stmt->fetch();
+    $stmt->close();
+    
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format.";
+    }else if($emailCount > 0){
+      $emailErr = "Email already exists.";
+    }else{
+      $sql = "INSERT INTO stockai_email(email, count, time) VALUES(?,0,NOW())";
+      $stmt = $conn->prepare($sql);
+      $stmt->bind_param("s",$email);
+      $stmt->execute();
+      $stmt->close();	
+      echo "success";
+      exit();
+    }
+    echo $emailErr;
+    exit();
 	}else if(isset($_POST['unsub'])){
-	    $email = $_POST['unsub'];
-	    $sql = "SELECT COUNT(id) FROM stockai_email WHERE email = ?";
-    	$stmt = $conn->prepare($sql);
-    	$stmt->bind_param("s",$email);
-    	$stmt->execute();
-    	$stmt->bind_result($emailCount);
-    	$stmt->fetch();
-    	$stmt->close();
-    	if($emailCount < 1){
-    	    echo "Email does not exist.";
-    	    exit();
-    	}
-    	$sql = "DELETE FROM stockai_email WHERE email=? LIMIT 1";
+    $email = $_POST['unsub'];
+    $sql = "SELECT COUNT(id) FROM stockai_email WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s",$email);
+    $stmt->execute();
+    $stmt->bind_result($emailCount);
+    $stmt->fetch();
+    $stmt->close();
+
+    if($emailCount < 1){
+      echo "Email does not exist.";
+      exit();
+    }
+    $sql = "DELETE FROM stockai_email WHERE email=? LIMIT 1";
 		$stmt = $conn->prepare($sql);
 		$stmt->bind_param("s",$email);
 		$stmt->execute();
@@ -60,38 +61,41 @@
 	}
 ?>
 <html>
-    <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        
-        <title>Stock AI</title>
-        
-        <meta lang="en">
-		<link rel="stylesheet" type="text/css" href="/style/style_ai.css">
-        <link rel="icon" type="image/x-icon" href="/images/favstock.png">
-        <meta name="description" content="An open-source service for analysing the most important Hungarian stocks.">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        	  <link rel="manifest" href="/manifest.json">
-        <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="apple-mobile-web-app-status-bar-style" content="#282828">
-        <meta name="apple-mobile-web-app-title" content="Pearscom">
-        <link rel="apple-touch-icon" href="/images/icons/icon-152x152.png">
-        <meta name="theme-color" content="#282828" />
-    </head>
-    <body>
-		<header>
-			<a href="/main.php">Stock AI</a>
-			<div class="headerRight">
-				<a href="/downloads.php">Downloads</a>
-				<a href="/cont.php">Contribute</a>
-				<a href="/about.php">Information</a>
-			</div>
-		</header>
-		<br><br><br><br>
-		<span id="emailText"></span>
-		<div id="outerEmail"></div>
-		<div id="status"></div>
-        <div class="hStocks" id="hStocks"></div>
-		<?php require_once 'template_pageBottom.php'; ?>
-        <script src="/analysis.js" type="text/javascript"></script>
-        <script src="/email.js" type="text/javascript"></script>
-    </body>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+  <title>Stock AI</title>
+
+  <meta lang="en">
+  <link rel="stylesheet" type="text/css" href="/style/style_ai.css">
+  <link rel="icon" type="image/x-icon" href="/images/favstock.png">
+  <meta name="description"
+    content="An open-source service for analysing the most important Hungarian stocks.">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="manifest" href="/manifest.json">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="#282828">
+  <meta name="apple-mobile-web-app-title" content="Pearscom">
+  <link rel="apple-touch-icon" href="/images/icons/icon-152x152.png">
+  <meta name="theme-color" content="#282828" />
+</head>
+
+<body>
+  <header>
+    <a href="/main.php">Stock AI</a>
+    <div class="headerRight">
+      <a href="/downloads.php">Downloads</a>
+      <a href="/cont.php">Contribute</a>
+      <a href="/about.php">Information</a>
+    </div>
+  </header>
+  <br><br><br><br>
+  <span id="emailText"></span>
+  <div id="outerEmail"></div>
+  <div id="status"></div>
+  <div class="hStocks" id="hStocks"></div>
+  <?php require_once 'template_pageBottom.php'; ?>
+  <script src="/analysis.js" type="text/javascript"></script>
+  <script src="/email.js" type="text/javascript"></script>
+</body>
 </html>
